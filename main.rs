7f13e3a3a85d33bf23fc0c0beff701693a34ce4d
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-08-25 18:24:44 -0300

    Usando If/Else como expressões

--- main.rs
+++ main.rs
@@ -1,5 +1,11 @@
 static EVER:u16 = 128;
-fn main() {
+
+fn soma(a:i32 , b:i32) -> i32{
+ a + b
+}
+
+
+fn main() { 
 
     const PI:f32 = 3.14;
     let variavel = 256;
@@ -8,4 +14,20 @@ fn main() {
     println!("O valor de PI é = {}", PI);
     println!("O valor da variável de nome verdadeiro é = {}", verdadeiro);
     println!("{} : tem o tamanho em memória de {} bytes", EVER, std::mem::size_of_val(&EVER));
+    println!("Soma tem o valor de = {}", soma(2,2));
+
+    let idade:u16 = 16;
+    let esta_autorizado:bool = true;
+
+    if idade >= 18 {
+        println!("Venda de bebida liberada.")
+    } else if idade >= 16 && esta_autorizado {
+        println!("Venda de bebida liberada sob autorizacao dos pais.")
+    } else {
+        println!("Venda de bebida recusada.")
+    }
+
+    let condicao = if PI == 3.14 { true } else { false};
+
+    println!("resultado eh = {}", condicao);
 }
