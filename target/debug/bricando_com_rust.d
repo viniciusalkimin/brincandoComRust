29773057b88030e1dfebbc127b0b6e789e3dbba7
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-08-30 13:02:14 -0300

    Adicionando cargo como builder e gerenciador de dependencia

---
+++ target/debug/bricando_com_rust.d
@@ -0,0 +1,1 @@
+/home/viniciusalkimin/Desktop/AluraWorkspace/rust/brincandoComRust/target/debug/bricando_com_rust: /home/viniciusalkimin/Desktop/AluraWorkspace/rust/brincandoComRust/src/main.rs
---
+++ target/debug/deps/bricando_com_rust-14bda38d2015b073.d
@@ -0,0 +1,5 @@
+/home/viniciusalkimin/Desktop/AluraWorkspace/rust/brincandoComRust/target/debug/deps/bricando_com_rust-14bda38d2015b073: src/main.rs
+
+/home/viniciusalkimin/Desktop/AluraWorkspace/rust/brincandoComRust/target/debug/deps/bricando_com_rust-14bda38d2015b073.d: src/main.rs
+
+src/main.rs:
